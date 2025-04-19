623fc4a4fcb5d8dc4a260429816060288fd3ea96
Artemi Ogolikhin <53339854+Artemyogo@users.noreply.github.com>
2025-04-20 00:53:00 +0200

    Adding MenuScreen class

--- core/src/main/java/com/tiktime/Main.java
+++ core/src/main/java/com/tiktime/Main.java
@@ -1,7 +1,10 @@
 package com.tiktime;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
+public class Main extends Game {
+    @Override
+    public void create() {
+
+    }
 }
---
+++ core/src/main/java/com/tiktime/view/View.java
@@ -0,0 +1,7 @@
+package com.tiktime.view;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public abstract class View {
+    private Stage stage;
+}
