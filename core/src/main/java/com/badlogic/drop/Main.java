dc5057e137edce15461bd47f215813f05042737c
Artemi Ogolikhin <53339854+Artemyogo@users.noreply.github.com>
2025-04-19 19:37:56 +0200

    Generating project template

---
+++ core/src/main/java/com/badlogic/drop/Main.java
@@ -0,0 +1,7 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+}
