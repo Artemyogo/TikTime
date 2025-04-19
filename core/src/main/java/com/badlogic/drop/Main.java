0232528501314b09d033777482f730287144ad18
Artemi Ogolikhin <53339854+Artemyogo@users.noreply.github.com>
2025-04-19 20:32:34 +0200

    Adding model view and controller folders

--- core/src/main/java/com/badlogic/drop/Main.java
+++
@@ -1,7 +0,0 @@
-package com.badlogic.drop;
-
-import com.badlogic.gdx.ApplicationAdapter;
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-}
