81224041c527679c89df45de3a1652a637816c1d
Zakhar Tsvirka <zakhar.tsvirko.2445@gmail.com>
2025-06-10 19:50:25 +0200

    Fixed Ak47WeaponModel avoiding unnecessary casts

--- core/src/main/java/com/tiktime/model/entities/weapons/Ak47WeaponModel.java
+++ core/src/main/java/com/tiktime/model/entities/weapons/Ak47WeaponModel.java
@@ -1,35 +1,29 @@
 package com.tiktime.model.entities.weapons;
 
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
 import com.tiktime.common.WeaponType;
-import com.tiktime.common.configs.GameConfig;
-import com.tiktime.common.configs.configdata.WeaponData;
-import com.tiktime.model.BodyManager;
-import com.tiktime.model.entities.components.AttackComponent;
 import com.tiktime.model.entities.components.GunshotAttackComponent;
 
-public class Ak47WeaponModel extends WeaponModel implements GunshotAttackable {
+public final class Ak47WeaponModel extends WeaponModel implements GunshotAttackable {
+    private final GunshotAttackComponent attackComponent;
+
     public Ak47WeaponModel(GunshotAttackComponent gunshotAttackComponent) {
         super(gunshotAttackComponent, WeaponType.AK47);
+        attackComponent = gunshotAttackComponent;
     }
 
     @Override
     public boolean tryAttack(float x, float y) {
-        GunshotAttackable gunshotAttackable = (GunshotAttackable) attackComponent;
-        return gunshotAttackable.tryAttack(x, y);
+        return attackComponent.tryAttack(x, y);
     }
 
 
     @Override
     public void updateWeaponRotation(float rotationDeg) {
-        GunshotAttackable gunshotAttackable = (GunshotAttackable) attackComponent;
-        gunshotAttackable.updateWeaponRotation(rotationDeg);
+        attackComponent.updateWeaponRotation(rotationDeg);
     }
 
     @Override
     public float getRotationDeg() {
-        GunshotAttackable gunshotAttackable = (GunshotAttackable) attackComponent;
-        return gunshotAttackable.getRotationDeg();
+        return attackComponent.getRotationDeg();
     }
 }
